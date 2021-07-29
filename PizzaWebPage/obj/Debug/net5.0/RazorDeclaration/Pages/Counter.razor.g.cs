fe893305c82d45654338c099aa02139c448a83f6
Julian Itwaru <jrk57j@gmail.com>
2021-07-29 19:44:28 -0400

    project build

--- PizzaWebPage/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ PizzaWebPage/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace PizzaWebPage.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 1 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 2 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 3 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 4 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 5 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 6 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 7 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 8 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 9 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using PizzaWebPage;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
+#line 10 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\_Imports.razor"
 using PizzaWebPage.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using PizzaWebPage.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\jitwa\Source\Repos\Jrk57j\Blazor\PizzaWebPage\Pages\Counter.razor"
+#line 9 "C:\Users\jitwa\source\repos\Jrk57j\Blazor\PizzaWebPage\Pages\Counter.razor"
        
     private int currentCount = 0;
 
